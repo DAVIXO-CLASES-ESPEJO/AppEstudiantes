c91d5823b691fe2363906efc846c0ade3f41b414
Davixo23 <david.utic@gmail.com>
2026-09-18 10:10:04 -0400

    davixo:feat(nav): controller de rutas

--- app/src/main/java/com/example/appestudiantes/presentation/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/appestudiantes/presentation/navigation/AppNavigation.kt
@@ -1,8 +1,32 @@
 package com.example.appestudiantes.presentation.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.appestudiantes.presentation.screens.LoginScreen
+import com.example.appestudiantes.presentation.screens.RegisterScreen
 
 @Composable
 fun AppNavigation(){
 
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = AppRoutes.Login
+    ){
+        composable<AppRoutes.Login>{
+            LoginScreen(onNavigateToRegister = {
+                navController.navigate(AppRoutes.Register)
+            })
+        }
+
+        composable<AppRoutes.Register>{
+            RegisterScreen(
+                onNavigateBackToLogin = {
+                    navController.popBackStack()
+                }
+            )
+        }
+    }
 }
--- app/src/main/java/com/example/appestudiantes/presentation/navigation/AppRoutes.kt
+++ app/src/main/java/com/example/appestudiantes/presentation/navigation/AppRoutes.kt
@@ -1,4 +1,11 @@
 package com.example.appestudiantes.presentation.navigation
 
+import kotlinx.serialization.Serializable
+
 sealed interface AppRoutes {
+    @Serializable
+    data object Login : AppRoutes
+
+    @Serializable
+    data object Register : AppRoutes
 }
