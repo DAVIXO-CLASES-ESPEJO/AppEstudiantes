f8eda3a05a54e3cce0a4723339a9e8ba92c1b8af
Davixo23 <david.utic@gmail.com>
2026-09-18 09:26:15 -0400

    davixo:feat(componentes): input y button

--- app/src/main/java/com/example/appestudiantes/presentation/components/CustomInput.kt
+++ app/src/main/java/com/example/appestudiantes/presentation/components/CustomInput.kt
@@ -1,8 +1,40 @@
 package com.example.appestudiantes.presentation.components
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.VisualTransformation
 
 @Composable
-fun CustomInput(){
-
+fun CustomInput(
+    // texto dentro del input
+    value: String,
+    // fun que cambia cada vez que se escribe algo dentro del input
+    onValueChanged:(String) -> Unit,
+    // titulo flotante
+    label:String,
+    // modificador opcional externo
+    modifier: Modifier = Modifier,
+    // tipo de teclado en el dispositivo
+    keyboardType: KeyboardType = KeyboardType.Text,
+    // efecto visual que oculta el password
+    visualTransformation: VisualTransformation = VisualTransformation.None,
+    // input de una sola linea
+    singleLine: Boolean = true,
+    // recibir un icono al final del input
+){
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChanged,
+        label = {Text(label)},
+        singleLine = singleLine,
+        visualTransformation = visualTransformation,
+        keyboardOptions = KeyboardOptions(keyboardType = keyboardType),
+        //trailingIcon = trailingIcon,
+        modifier = modifier.fillMaxWidth()
+    )
 }
--- app/src/main/java/com/example/appestudiantes/presentation/components/PrimaryButton.kt
+++ app/src/main/java/com/example/appestudiantes/presentation/components/PrimaryButton.kt
@@ -1,8 +1,50 @@
 package com.example.appestudiantes.presentation.components
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ModifierInfo
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
 
 @Composable
-fun PrimaryButton(){
+fun PrimaryButton(
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    isLoading: Boolean = false,
+    enabled: Boolean = true,
+    ){
+    Button(
+        onClick = onClick,
+        modifier = modifier
+            .fillMaxWidth()
+            .height(50.dp),
+        // borde redondeado
+        shape = MaterialTheme.shapes.medium,
+        enabled = enabled && !isLoading
+    ) {
+        // pregunta si el boton esta en estado carga o enabled
+        if(isLoading){
+            CircularProgressIndicator(
+                modifier = Modifier.size(24.dp),
+                color = MaterialTheme.colorScheme.onPrimary
+            )
+        }else{
+            Text(
+                text = text, // ingresar, eliminar aceptar
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }
+    }
 
 }
